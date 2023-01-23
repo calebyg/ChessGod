0edbfc53e54330971fe6eb612aa5cd6875b6638e
Caleb Ghirmai <calebyg15@gmail.com>
2023-01-22 23:09:47 -0800

    Added bot command to return full name of chess.com user

--- chess-god.py
+++ chess-god.py
@@ -1,23 +1,34 @@
-from dotenv import load_dotenv
-
-import discord
 import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+from chessdotcom import get_player_profile
 
 load_dotenv() # import variables from .env
 token = os.environ.get("api-token")
+bot = commands.Bot(command_prefix='$')
 
-intents = discord.Intents.all()
-client = discord.Client(command_prefix = '!', intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    message.content = message.content.lower() # makes all messages lowercase
+    if message.author == bot.user:
         return
-    if message.content.startswith('hi'):
-        await message.channel.send('Hello!')
+    if "hi" in message.content:
+        await message.channel.send('Hello!') # response
 
-client.run(token)
+    await bot.process_commands(message) # necessary to accept commands from users
+
+# Commands
+
+# Returns the full name of the chess username argument
+# ex: $test fabianocaruana - Fabiano Caruana
+@bot.command(aliases=['username'])
+async def name(ctx, arg):
+    response = get_player_profile(arg)
+    await ctx.send(f'Name: {response.player.name}')
+
+bot.run(token)
